2483b8d7cd5a8f19df2e78b4eb46f46531eed108
Deepak Sharma <dsharma2828@gmail.com>
2025-06-17 22:20:03 +0530

    Swagger Added

---
+++ user-service/src/main/java/com/inventory/userservice/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.inventory.userservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("User Service API")
+                        .version("1.0.0")
+                        .description("API documentation for the User Service in the Inventory Management System"));
+    }
+}
